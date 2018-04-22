f6e967a1e7f6d43de4f808f83aaba75184a35140
Bideo Wego <bideowego@gmail.com>
2018-04-21 22:18:18 -0400

    Add templates

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Home'
+  return render_template('index.html')
 
 @app.route('/hello')
-def hello():
-  return 'Hello'
+@app.route('/hello/<name>')
+def hello(name=None):
+  return render_template('index.html', name=name)
 
 if __name__ == '__main__':
   app.run()
